298284fa27f210efc72888ab1ebae1b70a595cde
Kuthumi Pepple <kuthumipepple@gmail.com>
2018-11-28 09:27:21 +0000

    Add add() method to linked_list.js

--- linked_list.js
+++ linked_list.js
@@ -14,4 +14,23 @@ function LinkedList() {
 	this.head = function(){
 		return head;
 	}
+
+	this.add = function(element){
+		let node = new Node(element);
+		// check if list is empty. If yes set head to point to new node
+		if(head === null){
+			head = node;
+		}else{
+			// create reference to head
+			let currentNode = head;
+			// then traverse linked list to reach end node
+			while(currentNode.next){
+				currentNode = currentNode.next;
+			}
+			// set end node's next property to new node
+			currentNode.next = node;
+		}
+		// increment size of linked list
+		length++;
+	}
 }
